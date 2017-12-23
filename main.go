55e80e2321a82716915c15ccaedd07e317f879f9
Dominic St-Pierre <dominicstpierre@gmail.com>
2017-12-23 07:24:06 -0500

    1-3 Dedicated function as HTTP handler

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
+	http.HandleFunc("/time", getTime)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, h *http.Request) {
 		w.Write([]byte("hello world"))
 	})
@@ -14,4 +17,8 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("unable to start web server", err)
 	}
+}
+
+func getTime(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(time.Now().String()))
 }
