17b0247028ea6619c2a70673d461c1402193b917
Dominic St-Pierre <dominicstpierre@gmail.com>
2017-12-23 06:45:52 -0500

    1-2 The hello world of HTTP request handler

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, h *http.Request) {
+		w.Write([]byte("hello world"))
+	})
+
 	log.Println("web server started at localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("unable to start web server", err)
